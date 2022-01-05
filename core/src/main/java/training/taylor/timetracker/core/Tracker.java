7cd8eb7c9993b46e3ad99076969b746f8b3a1834
Nina S <utbildning@familjenstarck.se>
2022-01-05 13:30:55 +0100

    Ändrat koden

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,56 +1,30 @@
-/**
- * Info about this package doing something for package-info.java file.
- */
 package training.taylor.timetracker.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/** Represents a tracker.
- * @author Nina
- * @version 1.0
- * @since 1.0
-*/
 @Component
 public class Tracker {
-    /**
-    * The list of entries to be used in class.
-    */
     @Autowired
-    private List<TimeEntry> entries;
+    private final List<TimeEntry> entries = new ArrayList<>();
 
-    /** Add a time entry.
-     * @param entry A time entry.
-    */
-    public void add(final TimeEntry entry) {
+    public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
-    /** Remove a time entry.
-     * @param entry A time entry.
-    */
-    public void remove(final TimeEntry entry) {
-        if (true) {
-            entries.remove(entry);
-        }
+    public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
-    
-    /** Return the size of all time entries.
-     * @return int A time entry.
-    */
+
     public int size() {
         return entries.size();
     }
 
-    /** Return a time entry on index i.
-     * @param index An index int.
-     * @return TimeEntry
-    */
-    public TimeEntry get(final int index) {
+    public TimeEntry get(int index) {
         return entries.get(index);
     }
 }
